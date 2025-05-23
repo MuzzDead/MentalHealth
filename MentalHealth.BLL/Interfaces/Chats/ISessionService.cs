dd24f38c3f87b0876a032cf9db2e8c9cc5038677
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-05-23 03:03:04 +0300

    upd-service-interface

---
+++ MentalHealth.BLL/Interfaces/Chats/ISessionService.cs
@@ -0,0 +1,13 @@
+﻿using MentalHealth.BLL.DTOs.Chats.ChatSession;
+
+namespace MentalHealth.BLL.Interfaces.Chats;
+
+public interface ISessionService
+{
+	Task DeleteSession(Guid sessionId);
+	Task UpdateSession(UpdateChatSessionDTO sessionDTO);
+	Task<Guid> CreateSession(CreateChatSessionDTO chatSessionDTO);
+	Task<ChatSessionDTO> GetSession(Guid sessionId);
+	Task<IEnumerable<ChatSessionDTO>> GetSessionByUserId(Guid userId);
+	Task<IEnumerable<ChatSessionDTO>> GetSessions();
+}
