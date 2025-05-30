f1ffda138509373f682fe8934cda38c73a15002c
MuzzDead <98969533+MuzzDead@users.noreply.github.com>
2025-05-31 02:25:22 +0300

    added-message-controller

---
+++ MentalHealth.BLL/DTOs/Chats/ChatMessage/UpdateChatMessageDTO.cs
@@ -0,0 +1,16 @@
+﻿using MentalHealth.DAL.Enums.Chat;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MentalHealth.BLL.DTOs.Chats.ChatMessage;
+
+public class UpdateChatMessageDTO
+{
+	public Guid Id { get; set; }
+	public Guid SessionId { get; set; }
+	public Sender Sender { get; set; }
+	public string Content { get; set; } = string.Empty;
+}
